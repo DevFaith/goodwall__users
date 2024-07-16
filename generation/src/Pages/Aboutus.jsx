6ed247c7c441049c71958f758d84a3dc61ae99d2
DevFaith <waitherafaith@outlook.com>
2024-07-17 02:39:52 +0300

    * feat(Aboutus.jsx): add Aboutus component with Collaborate image

---
+++ generation/src/Pages/Aboutus.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Collaborate from '../assets/collaborate.jpg' 
+
+function Aboutus() {
+  return (
+    <div>
+      <img src={Collaborate} alt="" className='w-full '/>
+    </div>
+  )
+}
+
+export default Aboutus
